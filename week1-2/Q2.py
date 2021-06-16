9902f0bffaf5210cb62b114943c0d837e670122d
Allebhonk <85605371+Allebhonk@users.noreply.github.com>
2021-06-16 16:13:42 +0800

    Create Q2.py

---
+++ week1-2/Q2.py
@@ -0,0 +1,4 @@
+event = input("Input the event name: ") # asks for event name
+time = input("Input the time of the event in form time am/pm month date: ") # asks for time in specific format
+print("Event: {}, Time: {}".format(event, time)) # prints the event and time
+      
